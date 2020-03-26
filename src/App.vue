3a07326ae2c1eac185741d358c63b49cc593f630
wenzy <504706316@qq.com>
2020-03-26 15:23:47 +0800

    4-11Mock设置

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,16 @@
 // import storage from './storage'
 
 export default {
+  data() {
+    return {
+      res: {}
+    }
+  },
   mounted() {
+    // 本地集成mockjs
+    this.axios.get('/user/login').then((res) => {
+      this.res = res;
+    })
     // storage.setItem("user", {"a": 3})
     // storage.setItem("abc", {"aaa": 3}, "user")
     // storage.clear("a", "user")
--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import env from './env'
+// import env from './env'
 
+const mock = true
+if (mock) {
+  require('./mock/api')
+}
 // 默认配置 - 根据前端的跨域方式做调整 此处是用代理的方式，如果用jsonp或cros的需要加上域名如 https://www.a.com
 axios.defaults.baseURL = '/api';
 // 默认配置 - 超时
 axios.defaults.timeout = 8000;
-axios.defaults.baseURL = env.baseURL
+// axios.defaults.baseURL = env.baseURL
 // 接口错误拦截
 axios.interceptors.response.use(function(response) {
   let res = response.data;
